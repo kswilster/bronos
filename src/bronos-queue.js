cc77645c901baed747f1405089308da4c668d87c
Keith Williams <kwilliams@salsify.com>
2016-09-28 23:10:14 -0400

    versatility

--- src/bronos-queue.js
+++ src/bronos-queue.js
@@ -5,23 +5,113 @@ var Sonos = require('sonos');
 var program = require('commander');
 var spawn = require('child_process').spawn;
 
-const app = {
-  run: async function(searchTerm) {
-    const spotifyApi = new SpotifyWebApi();
-    const { body: { tracks: { items } } } = await spotifyApi.search(searchTerm, ['track'], {
-      limit: 50
-    });
+const spotifyApi = new SpotifyWebApi();
 
+const app = {
+  run(query, { type='track' }) {
+    if (type === 'artist') {
+      this.searchByArtist(query);
+    } else if (type === 'album') {
+      this.searchByAlbum(query);
+    } else {
+      this.searchByTrack(query);
+    }
+  },
+
+  searchByArtist: async function(query) {
+    const artists = await this.findArtists(query);
+    const artistId = await this.chooseArtist(artists);
+    const albums = await this.getArtistAlbums(artistId);
+    const albumId = await this.chooseAlbum(albums);
+    const tracks = await this.getAlbumTracks(albumId);
+    const trackId = await this.chooseTrack(tracks);
+    console.log(trackId);
+  },
+
+  searchByAlbum: async function(query) {
+    const albums = await this.findAlbums(query);
+    const albumId = await this.chooseAlbum(albums);
+    const tracks = await this.getAlbumTracks(albumId);
+    const trackId = await this.chooseTrack(tracks);
+    console.log(trackId);
+  },
+
+  searchByTrack: async function(query) {
+    const tracks = await this.findTracks(query);
+    const trackId = await this.chooseTrack(tracks);
+  },
+
+  chooseArtist: async function(artists) {
+    var fzfInput = '';
+
+    artists.forEach(function(artist, index) {
+      fzfInput += `${index}: ${artist.name}\n`;
+    })
+
+    const result = await this.startFzf(fzfInput);
+    const index = result.split(':')[0];
+    const artist = artists[index];
+    return artist.id;
+  },
+
+  chooseAlbum: async function(albums) {
+    var fzfInput = '';
+
+    albums.forEach(function(album, index) {
+      fzfInput += `${index}: ${album.name}\n`;
+    })
+
+    const result = await this.startFzf(fzfInput);
+    const index = result.split(':')[0];
+    const album = albums[index];
+    return album.id;
+  },
+
+  chooseTrack: async function(tracks) {
     var fzfInput = "";
 
-    items.forEach(function(track, index) {
+    tracks.forEach(function(track, index) {
       fzfInput += `${index}: ${track.artists[0].name}\t\t${track.name}\n`;
     });
 
     const result = await this.startFzf(fzfInput);
     const index = result.split(':')[0];
-    const track = items[index];
-    console.log(track.id);
+    const track = tracks[index];
+    return track.id;
+  },
+
+  /**
+   * lower level spotifyApi wrappers
+   */
+   findArtists: async function(query) {
+     const { body: { artists: { items } } } = await spotifyApi.search(query, ['artist'], {
+       limit: 50
+     });
+     return items;
+   },
+
+   findAlbums: async function(query) {
+     const { body: { albums: { items } } } = await spotifyApi.search(query, ['album'], {
+       limit: 50
+     });
+     return items;
+   },
+
+  findTracks: async function(query) {
+    const { body: { tracks: { items } } } = await spotifyApi.search(query, ['track'], {
+      limit: 50
+    });
+    return items;
+  },
+
+  getArtistAlbums: async function(artistId) {
+    const { body: { items } } = await spotifyApi.getArtistAlbums(artistId);
+    return items;
+  },
+
+  getAlbumTracks: async function(albumId) {
+    const { body: { items } } = await spotifyApi.getAlbumTracks(albumId);
+    return items;
   },
 
   startFzf: async function(entries) {
@@ -46,13 +136,12 @@ const app = {
 }
 
 // TODO: handle insert at index
-// TODO: handle searching for artists
 
 program
   .arguments('<query...>')
   .option('-i, --index [<index>]', 'queue insertion index')
-  .option('-t, --type [<types>]', 'types of entities')
+  .option('-t, --type [<type>]', 'type of entity to search for (artist | album | track). Defaults to track')
   .action(function(query, options) {
-    app.run(query.join(' '));
+    app.run(query.join(' '), options);
   })
   .parse(process.argv);
