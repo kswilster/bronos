16ca6958bf41f5e0cabad97b5a589b93d21a0aa4
Keith Williams <kwilliams@salsify.com>
2016-09-29 16:20:20 -0400

    etc

--- src/bronos.js
+++ src/bronos.js
@@ -5,4 +5,5 @@ var program = require('commander');
 program
   .command('play [query]', 'play one or more songs immediately')
   .command('queue [query]', 'queue one or more songs')
+  .command('list', 'list available devices and groups')
   .parse(process.argv);
