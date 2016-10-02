0c41cfed61448cc25ffdba6a9b46baf8fbd016a6
Keith Williams <kwilliams@salsify.com>
2016-10-01 20:24:56 -0400

    fix publish, omit some commands

--- src/bronos.js
+++ src/bronos.js
@@ -4,9 +4,9 @@ require('babel-polyfill');
 var program = require('commander');
 
 program
-  .command('play <query>', 'play one or more songs immediately')
+  // .command('play <query>', 'play one or more songs immediately')
   .command('queue <query>', 'queue one or more songs')
-  .command('list', 'list available devices and groups')
+  // .command('list', 'list available devices and groups')
   .command('status', 'show current device status')
   .command('use', 'select a device or group')
   .parse(process.argv);
