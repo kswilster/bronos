be2068b1b2556a926a2d14f9ff0cabce34add9b7
Keith Williams <kwilliams@salsify.com>
2017-06-08 22:52:34 -0400

    fauxAjax over moxios

--- src/test/models/spotify-api-test.js
+++ src/test/models/spotify-api-test.js
@@ -1,45 +1,47 @@
 import sinon from 'sinon';
-import moxios from 'moxios';
+import fauxJax from 'faux-jax';
 import assert from 'assert';
 import Preferences from 'preferences';
-import SpotifyApi from 'models/spotify-api';
+import SpotifyApi from '~/models/spotify-api';
 
 let fakeTokenServer, tokenSpy;
 
 describe('spotify-api', function() {
   before(function() {
-    moxios.install()
+    fauxJax.install();
   });
 
   after(function() {
-    moxios.uninstall()
+    fauxJax.restore();
   });
 
   describe('#authenticate without established preferences', function() {
     const preferences = {};
-    const spotifyApi = SpotifyApi.create({
-      _preferences: preferences
-    });
+    const spotifyApi = SpotifyApi.create({ _preferences: preferences });
 
-    it('calls the token server and caches the response', function(done) {
-      spotifyApi.authenticate();
 
-      moxios.wait(function () {
-        let request = moxios.requests.mostRecent();
-        request.respondWith({
-          status: 200,
-          response: {
-            body: {
-              access_token: 'foo',
-              expires_in: 1234,
-            },
-          },
-        }).then(function () {
-          assert.equal(spotifyApi.get('accessToken'), 'foo');
-          assert.equal(preferences.tokenCache.token, 'foo');
-          done();
-        });
-      });
+    it('calls the token server and caches the response', async function() {
+      const respond = sinon.spy(jsonResponse({
+        body: {
+          access_token: 'foo',
+          expires_in: 1234,
+        }
+      }));
+      fauxJax.on('request', respond);
+
+      await spotifyApi.authenticate();
+
+      assert.equal(spotifyApi.get('accessToken'), 'foo');
+      assert.equal(preferences.tokenCache.token, 'foo');
+      assert.ok(respond.calledOnce);
     });
   });
 });
+
+function jsonResponse(data) {
+  return (request) => {
+    const status = 200;
+    const headers = { 'Content-Type': 'application/json' };
+    request.respond(status, headers, JSON.stringify(data));
+  };
+}
